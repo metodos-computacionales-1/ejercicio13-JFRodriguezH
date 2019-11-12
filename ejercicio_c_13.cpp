3887e32e448fd57ab3c705e655b1d3e2ffa346de
Default user <jf.rodriguezh@uniandes.edu.co>
2019-11-12 13:11:41 +0000

    Correcciones

--- ejercicio_c_13.cpp
+++ ejercicio_c_13.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 
-int producto(int m[][2], int n[][2]);
 
 int main(){
     int m[2][2] = {{2,2},{3,3}};
     int n[2][2] = {{1,1},{5,5}};
-
-    producto(m,n);
-    for(int i=0; i<2;i++){
-        for(int j=0; j<2; j++){
-            std::cout<< m[i][j] << " ";
+    int f_m = 2;
+    int c_m = 2;
+    int f_n = 2;
+    int c_n = 2;
+    int mult[f_m][c_n];
+    for(int i=0; i < f_m; i++){
+        for(int j=0; j < c_n; j++){
+            mult[i][j]=0;
+        }
+    }
+    
+    for(int i=0; i < f_m; i++){
+        for(int j=0; j < c_n; j++){
+            std::cout<< mult[i][j] << " ";
         }
         std::cout<<std::endl;
     }
-    return 0;
-}
-
-int producto(int m[][2], int n[][2]){
-    int f_m = 2;
-    int c_m = 2;
-    
-    int f_n = 2;
-    int c_n = 2;
     
     for(int i=0;i<f_m;i++){
         for(int j=0;j<c_n;j++){
-            int suma = 0;
+            int suma=0;
             for(int k=0;k<c_m;k++){
-                suma += m[i][k]*n[k][j];
+                suma += m[i][k] * n[k][j];
             }
-            m[i][j] = suma;
+            mult[i][j]=suma;
         }
     }
     
+    for(int i=0; i < f_m; i++){
+        for(int j=0; j < c_n; j++){
+            std::cout<< mult[i][j] << " ";
+        }
+        std::cout<<std::endl;
+    }
     return 0;
 }
